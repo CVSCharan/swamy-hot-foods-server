b971687df318da7217682cc685bf78a4fb39d88f
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2024-12-03 14:38:21 +0530

    final changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const WebSocket = require('ws');
+const express = require("express");
+const WebSocket = require("ws");
 const app = express();
 const port = 3001;
 
@@ -10,12 +10,12 @@ const wss = new WebSocket.Server({ noServer: true });
 let shopStatus = false; // Initial status
 
 // Set up WebSocket connection
-wss.on('connection', (ws) => {
+wss.on("connection", (ws) => {
   // Send the current status to the new client
   ws.send(JSON.stringify({ shopStatus }));
 
   // Listen for status changes from any client
-  ws.on('message', (message) => {
+  ws.on("message", (message) => {
     const data = JSON.parse(message);
     if (data.shopStatus !== undefined) {
       shopStatus = data.shopStatus; // Update the status
@@ -29,13 +29,17 @@ wss.on('connection', (ws) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Swamy Hot Foods Server");
+});
+
 // Set up HTTP server to handle WebSocket upgrades
 app.server = app.listen(3001, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.server.on('upgrade', (request, socket, head) => {
+app.server.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
-    wss.emit('connection', ws, request);
+    wss.emit("connection", ws, request);
   });
 });
