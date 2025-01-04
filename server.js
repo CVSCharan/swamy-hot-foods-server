b71c9d9371ba96f2ed4f678a8e8ae45facc534b8
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2025-01-05 00:22:59 +0530

    added Current Status Msg

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const path = require("path");
 const axios = require("axios");
 const app = express();
-const port = 4001;
+const port = 3001;
 
 // Use CORS to allow all origins (or specify frontend domain if required)
 const allowedOrigins = [
