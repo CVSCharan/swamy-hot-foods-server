8854ce82e1d4ceab975a499b1a7f6407900710a6
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2025-01-21 19:29:30 +0530

    modified Menu Model

--- models/Menu.js
+++ models/Menu.js
@@ -5,7 +5,9 @@ const menuSchema = new mongoose.Schema(
     name: { type: String, required: true },
     price: { type: Number, required: true }, // Price of the menu item
     desc: { type: String, required: true }, // Description of the menu item
-    quant: { type: Number, required: true }, // Quantity available
+    timings: { type: String, required: true },
+    ingredients: { type: String, required: true },
+    priority: { type: Number, required: true },
     imgSrc: { type: String, required: true }, // Image URL for the menu item
   },
   {
